b14e0df936a767cb57b33fc3991f71c35756aff2
lijianguo1211 <risesli@hotmail.com>
2019-05-30 23:24:07 +0800

    '显示页面修改'

--- resources/views/public/reight.blade.php
+++ resources/views/public/reight.blade.php
@@ -1,7 +1,7 @@
 <aside class="col-md-4 blog-sidebar">
     <div class="p-3 mb-3 bg-light rounded">
         <h4>每日一句</h4>
-        <p><em>{{ $reghtTops['title'] }}&nbsp&nbsp</em>{{ $reghtTops['content'] }}</p>
+        <p><em>{{ $reghtTops['title'] }}&nbsp&nbsp</em><?php echo $reghtTops['content'] ?></p>
     </div>
 
     <div class="p-3">
