5fc1d3282d2c726ddda003ba20171e3a450ecb19
Jay.Li <jay.li@feisu.com>
2019-06-14 09:22:59 +0800

    '控制台输出'

--- app/Console/Commands/CreateRunTable.php
+++ app/Console/Commands/CreateRunTable.php
@@ -11,6 +11,8 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
+use App\Admin\Controllers\BlogController;
+use Symfony\Component\VarDumper\Caster\ReflectionCaster;
 
 class CreateRunTable extends Command
 {
@@ -36,19 +38,27 @@ class CreateRunTable extends Command
     public function handle()
     {
         try {
-            for ($i=0; $i<1000000000; $i++) {
-                $code = DB::table('tests')->insertGetId([
-                    'user' => $this->getTest(45),
-                    'phone' => $this->getTest(10),
-                    'email' => $this->getTest(15),
-                    'info' => $this->getTest(150),
-                ]);
-                $this->info($code . '-->');
-            }
+            $this->getInstance();
         } catch (\Exception $e) {
             $this->error($e->getMessage());
         }
 
+    }
+
+    public function getInstance()
+    {
+        $ref = new \ReflectionClass(BlogController::class);
+        $this->info('*************构造函数*************');
+        $con = $ref->getConstructor();
+        var_dump($con);
+        $this->info('*************构造函数*************');
+        $this->info('*************构造函数参数*************');
+        $par = $con->getParameters();
+        var_dump($par);
+        $this->info('*************构造函数参数*************');
+        $this->info('*************实例化输出*************');
+        $class = $ref->newInstanceArgs($par);
+        var_dump($class);
     }
 
     public function getTest($strl)
@@ -60,7 +70,15 @@ class CreateRunTable extends Command
             $first = mt_rand(0, $len-1);
             $code .= $str[$first];
         }
-
+        for ($i=0; $i<1000000000; $i++) {
+            $code = DB::table('tests')->insertGetId([
+                'user' => $this->getTest(45),
+                'phone' => $this->getTest(10),
+                'email' => $this->getTest(15),
+                'info' => $this->getTest(150),
+            ]);
+            $this->info($code . '-->');
+        }
         return $code;
     }
 }
