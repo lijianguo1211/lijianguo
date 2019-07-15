a5b7f9d3c56b3f6fb9e8911e1490c0f3461bbb67
Jay.Li <jay.li@feisu.com>
2019-07-15 18:45:57 +0800

    'dingapi‘

---
+++ app/Api/V1/Controllers/BaseController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Notes:
+ * File name:BaseController
+ * Create by: Jay.Li
+ * Created on: 2019\7\15 0015 14:52
+ */
+
+
+namespace App\Api\V1\Controllers;
+
+use Dingo\Api\Routing\Helpers;
+use Illuminate\Routing\Controller;
+
+class BaseController extends Controller
+{
+    use Helpers;
+}
