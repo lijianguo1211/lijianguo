eeb1ef735df7c539f747796825e7ed63cb5d087e
lijianguo1211 <risesli@hotmail.com>
2019-09-08 22:44:43 +0800

    change

--- resources/js/components/FooterComponent.vue
+++ resources/js/components/FooterComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="blog-footer">
-        <p>php_liyi<a href=""></a></p>
+        <p><a href="http://www.lglg.xyz">php-liyi</a></p>
         <p>
             <span>Copyright © 花儿尊上&nbsp&nbsp</span><a href="http://www.miibeian.gov.cn/">鄂ICP备18021322号-1</a>
         </p>
